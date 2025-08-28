76e26b7c0160d2cda33a3b77debfcdecd9256e6a
Hussam Albasha <hussam.dev.001@gmail.com>
2025-08-28 14:43:45 +0300

    fix: sign in screen

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -56,135 +56,139 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        spacing: 100,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-
-          Flexible(
-            flex: 1,
-
-            child: Hero(
-              tag: 'logo',
-              child:
-              Image.asset(
-                'assets/images/logo3.png',
-
-                fit: BoxFit.contain,
-
-              ),
-
-            ),
-          ),
-
-          Flexible(
-            flex: 2,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade100,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.sizeOf(context).height / 2,
               ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 30),
-                    child: Text(
-                      "Sign In",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: kPrimaryColor,
-                        fontSize: 30,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            textDirection: TextDirection.ltr,
-                            keyboardType: TextInputType.emailAddress,
-                            onChanged: (value) {
-                              email = value;
-                            },
-                            decoration: kTextFieldDecoration.copyWith(
-                              alignLabelWithHint: true,
-                              labelText: "Email",
-                              prefixIcon: const Icon(Icons.person),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return '';
-                              }
-                              return null;
-                            },
-                          ),
-                          const SizedBox(height: 15.0),
-                          TextFormField(
-                            textDirection: TextDirection.ltr,
-                            obscureText: true,
-                            onChanged: (value) {
-                              password = value;
-                            },
-                            decoration: kTextFieldDecoration.copyWith(
-                              labelText: "Password",
-                              prefixIcon: const Icon(Icons.lock),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return '';
-                              }
-                              return null;
-                            },
-                          ),
-                          errorMessage != null
-                              ? Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 15.0,
-                                  ),
-                                  child: Text(
-                                    errorMessage!,
-                                    style: const TextStyle(color: Colors.red),
-                                  ),
-                                )
-                              : const SizedBox(height: 15),
-                          showSpinner
-                              ? Center(
-                                  child: CircularProgressIndicator(
-                                    color: kPrimaryColor,
-                                  ),
-                                )
-                              : ElevatedButton(
-                                  onPressed: handleSignIn,
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: kPrimaryColor,
-                                    foregroundColor: Colors.white,
-                                    minimumSize: const Size.fromHeight(40),
-                                  ),
-                                  child: const Text(
-                                    "Sign In",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ),
-                        ],
-                      ),
+                  Hero(
+                    tag: 'logo',
+                    child: Image.asset(
+                      'assets/images/logo3.png',
+                      fit: BoxFit.contain,
+                      height: 300,
                     ),
                   ),
                 ],
-               ),
               ),
-          ),
-           ],
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.sizeOf(context).height / 2,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade500,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 30),
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: kPrimaryColor,
+                          fontSize: 30,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              textDirection: TextDirection.ltr,
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                email = value;
+                              },
+                              decoration: kTextFieldDecoration.copyWith(
+                                alignLabelWithHint: true,
+                                labelText: "Email",
+                                prefixIcon: const Icon(Icons.person),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return '';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 15.0),
+                            TextFormField(
+                              textDirection: TextDirection.ltr,
+                              obscureText: true,
+                              onChanged: (value) {
+                                password = value;
+                              },
+                              decoration: kTextFieldDecoration.copyWith(
+                                labelText: "Password",
+                                prefixIcon: const Icon(Icons.lock),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return '';
+                                }
+                                return null;
+                              },
+                            ),
+                            errorMessage != null
+                                ? Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 15.0,
+                                    ),
+                                    child: Text(
+                                      errorMessage!,
+                                      style: const TextStyle(color: Colors.red),
+                                    ),
+                                  )
+                                : const SizedBox(height: 15),
+                            showSpinner
+                                ? Center(
+                                    child: CircularProgressIndicator(
+                                      color: kPrimaryColor,
+                                    ),
+                                  )
+                                : ElevatedButton(
+                                    onPressed: handleSignIn,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: kPrimaryColor,
+                                      foregroundColor: Colors.white,
+                                      minimumSize: const Size.fromHeight(40),
+                                    ),
+                                    child: const Text(
+                                      "Sign In",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
